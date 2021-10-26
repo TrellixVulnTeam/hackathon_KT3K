fcbea269bb8e79823b6581dff77e72cefe111a20
Husam <husam.m.hamdan@gmail.com>
2021-10-26 09:29:46 -0400

    Create shared py script

---
+++ mongodb.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+from flask import Flask, json
+
+def insertIntoDB(): 
+    uri = "mongodb+srv://admin:admin@hackathon.uj373.mongodb.net/hackathon?retryWrites=true&w=majority"
+    client = MongoClient(uri)
+    hackathon = client.hackathon
+    workoutData = hackathon.workoutData
+
+    emp_rec1 = {
+            "name":"Mr.Geek",
+            "eid":25,
+            "location":"delhi"
+            }
+    workoutData.insert_one(emp_rec1)
+
+
+companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
+api = Flask(__name__)
+
+@api.route('/workoutoptions', methods=['GET'])
+def get_companies():
+  return json.dumps(companies)
+
+if __name__ == '__main__':
+    insertIntoDB()
+    api.run() 
