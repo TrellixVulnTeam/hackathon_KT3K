3232a83a54b53d4450ed74a4242be03aa5ea173f
Nathan Glikman <nster98@gmail.com>
2021-10-26 15:47:04 -0400

    Update mongodb.py

--- mongodb.py
+++ mongodb.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
 from flask import Flask, json
+from werkzeug.wrappers import request
+from bson.json_util import dumps, loads
 
-def insertIntoDB(): 
+def insertWorkoutDataIntoDB(): 
     uri = "mongodb+srv://admin:admin@hackathon.uj373.mongodb.net/hackathon?retryWrites=true&w=majority"
     client = MongoClient(uri)
     hackathon = client.hackathon
@@ -14,24 +16,53 @@ def insertIntoDB():
       workoutData.insert_one(i)
 
     postWorkoutJSON.close()
+def insertListOfLoansIntoDB():
+  uri = "mongodb+srv://admin:admin@hackathon.uj373.mongodb.net/hackathon?retryWrites=true&w=majority"
+  client = MongoClient(uri)
+  hackathon = client.hackathon
+  listOfLoans = hackathon.loans
 
-companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"},
-              {"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"},
-              {"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"},
-              {"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"},
-              {"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"},
-              {"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"},
-              {"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"},
-              {"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"},
-              {"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"},
-              {"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"},
-              {"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
+  loanJSON = open('listOfLoans.json')
+  loans = json.load(loanJSON)
+  
+  for i in loans:
+    listOfLoans.insert_one(i)
+
+  loanJSON.close()
+
+def cleanDB():
+  uri = "mongodb+srv://admin:admin@hackathon.uj373.mongodb.net/hackathon?retryWrites=true&w=majority"
+  client = MongoClient(uri)
+  hackathon = client.hackathon
+
+  hackathon.workoutData.drop()
+  hackathon.loans.drop()
+
+companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
 api = Flask(__name__)
 
-@api.route('/workoutoptions', methods=['GET'])
+@api.route('/test', methods=['GET'])
 def get_companies():
   return json.dumps(companies)
 
+@api.route('/workoutoptions', methods=['GET'])
+def get_workout():
+  uri = "mongodb+srv://admin:admin@hackathon.uj373.mongodb.net/hackathon?retryWrites=true&w=majority"
+  client = MongoClient(uri)
+  hackathon = client.hackathon
+  workoutData = hackathon.workoutData
+
+  query = {"workoutSchema" : "workoutoptions"}
+
+  document = workoutData.find(query)
+  
+  list_current = list(document)
+  d = dumps(list_current)
+
+  return d
+
 if __name__ == '__main__':
-    ##insertIntoDB()
+    ##cleanDB()
+    ##insertListOfLoansIntoDB()
+    ##insertWorkoutDataIntoDB()
     api.run() 
